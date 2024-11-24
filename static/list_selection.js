60a6b98450b6465a28a464ec3d227592aef9c066
emunozgutier <emunozgutier@gmail.com>
2024-11-24 15:48:37 -0800

    Adds remove from filter-field

--- static/list_selection.js
+++ static/list_selection.js
@@ -10,23 +10,26 @@ $(document).ready(function() {
         if ($(this).hasClass('data-header-item')) {
             console.log('This item belongs to data-header-item');
             handleDataHeaderItemClick($(this));
-        }
-
-        if ($(this).hasClass('filter-field')) {
+        } else if ($(this).hasClass('filter-field')) {
             console.log('This item belongs to filter-field');
-            handleFilterFieldClick($(this));
+            handleFilterFieldClick($(this), $listItems);
         }
+    });
 
-        $listItems.removeClass('active');
-        console.log('Removed active class from all list items');
-
-        $(this).addClass('active');
-        console.log('Added active class to clicked list item');
+    $(document).on('change', '.filter-field-list-item-check', function() {
+        console.log('Checkbox state changed:', $(this).is(':checked'));
+        handleCheckboxChange($(this));
     });
 });
 
-function handleFilterFieldClick($item) {
+function handleFilterFieldClick($item, $listItems) {
     console.log('Handling filter-field item:', $item.text().trim());
+
+    $listItems.removeClass('active');
+    console.log('Removed active class from all list items');
+
+    $item.addClass('active');
+    console.log('Added active class to clicked list item');
 
     const $listElements = $item.find('.filter-field-list .list-group-item');
     $listElements.each(function() {
@@ -37,10 +40,25 @@ function handleFilterFieldClick($item) {
 function handleDataHeaderItemClick($item) {
     console.log('Handling data-header-item:', $item.text().trim());
 
-    const $filterFieldLists = $('.filter-field-list');
-    $filterFieldLists.each(function() {
-        const $newItem = $('<li class="list-group-item"></li>').text($item.text().trim());
+    const $activeFilterFieldLists = $('.filter-field.active .filter-field-list');
+    $activeFilterFieldLists.each(function() {
+        const $newItem = $('<div class="row"><div class="col"><input class="form-check-input filter-field-list-item-check" type="checkbox" value="" checked><li class="list-group-item filter-field-list-item"></li></div></div>');
+        $newItem.find('li').text($item.text().trim());
         $(this).append($newItem);
-        console.log('Added item to filter-field-list:', $newItem.text().trim());
+        console.log('Added item to active filter-field-list:', $newItem.find('li').text().trim());
     });
+}
+
+function handleFilterFieldListItemClick($item) {
+    console.log('Filter field list item clicked:', $item.text().trim());
+    $item.remove();
+    console.log('Removed filter field list item');
+}
+
+function handleCheckboxChange($checkbox) {
+    if (!$checkbox.is(':checked')) {
+        const $listItem = $checkbox.closest('.row');
+        console.log('Checkbox unchecked, removing list item:', $listItem.find('li').text().trim());
+        $listItem.remove();
+    }
 }
