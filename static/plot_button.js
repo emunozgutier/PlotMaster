9526625f9dbde0d10497f5ac19ecd3b56c8d63be
emunozgutier <emunozgutier@gmail.com>
2024-11-25 08:48:28 -0800

    Successfully sends the data

--- static/plot_button.js
+++ static/plot_button.js
@@ -1,18 +1,30 @@
-var $j = jQuery.noConflict();
-
-        $j(document).ready(function() {
-            $j('#plotButton').on('click', function() {
-                console.log('Plot button clicked');
-                $j.ajax({
-                    url: '/plot',
-                    type: 'POST',
-                    success: function(response) {
-                        console.log('Plot request successful');
-                        $j('#myTabContent').html(response.chart);
-                    },
-                    error: function(error) {
-                        console.log('Error:', error);
-                    }
-                });
-            });
+$(document).ready(function() {
+    $('#plotButton').on('click', function() {
+        console.log('Plot button clicked');
+        // Collect values from filter-field-list-items
+        var filterValues = [];
+        $('.filter-field-list-item').each(function() {
+            var $filterFieldName = $(this).closest('.row') ;
+            var $filterFieldName = $filterFieldName.parent().parent()
+            var $filterFieldName = $filterFieldName.children().first().text().trim();
+            var $filterFieldValue = $(this).text().trim();
+            // combines filter field name and value
+            var filterc = $filterFieldName + ':' + $filterFieldValue
+            filterValues.push(filterc);
         });
+
+        $.ajax({
+            url: '/plot',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ filters: filterValues }),
+            success: function(response) {
+                console.log('Plot request successful');
+                $('#myTabContent').html(response.chart);
+            },
+            error: function(error) {
+                console.log('Error:', error);
+            }
+        });
+    });
+});
