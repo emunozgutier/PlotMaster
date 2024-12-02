4cdab8e25d72f90d9c893e0cfb61a259f4f8f1f1
emunozgutier <emunozgutier@gmail.com>
2024-12-01 19:38:32 -0800

    Adds good plotly

--- static/js/TouchPlotPlotly.js
+++ static/js/TouchPlotPlotly.js
@@ -1,4 +1,4 @@
-class touchPlotPlotly{
+class touchPlotPlotly {
     /***************************************************************************
      * This class is responsible for handling the table data.
      *
@@ -9,22 +9,38 @@ class touchPlotPlotly{
     constructor(root) {
         this.root = root;
         this.main();
+        this.json_axis = "":
     }
-    main(){
 
-        const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
-        const yArray = [55, 49, 44, 24, 15];
 
-        const data = [{
-          x:xArray,
-          y:yArray,
-          type:"bar",
-          orientation:"v",
-          marker: {color:"rgba(0,0,255,0.6)"}
-        }];
+        const layout = {title: "World Wide Wine Production"};
 
-        const layout = {title:"World Wide Wine Production"};
-
-        Plotly.newPlot("tp-plot-plotly-div", data, layout);}
+        Plotly.newPlot("tp-plot-plotly-div", data, layout);
     }
+
+
+    }
+
+    updatePlot() {
+        this.json_axis = this.root.tpAxis.getAxis();
+        // check that x-axis and y-axis are not empty
+        if (this.json_axis.x_axis.length > 0 && this.json_axis.y_axis.length > 0) {
+            this.json_axis.y_axis.forEach((yvalue) =>{
+                const xArray = this.root.tpTable.df[this.json_axis.x_axis[0]].values;
+                const yArray = this.root.tpTable.df[yvalue].values;
+
+                const data_row = {
+                    x: xArray,
+                    y: yArray,
+                    type: "line",
+                    name: yvalue,
+                };
+                data.push(data_row);
+            })
+
+            const layout = {title: "plot"};
+            Plotly.newPlot("tp-plot-plotly-div", data, layout);
+        }
+    }
+}
 export default touchPlotPlotly;
